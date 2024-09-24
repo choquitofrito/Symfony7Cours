1d01b1cb53ef61dbbb855ae695ec94a2584e665c
choquitofrito <zyriab@gmail.com>
2024-09-24 11:39:41 +0200

    form recherche ajax

--- ProjetFormulairesSymfony/src/Controller/FormSearchLivresFiltresController.php
+++ ProjetFormulairesSymfony/src/Controller/FormSearchLivresFiltresController.php
@@ -8,11 +8,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class FormSearchLivresFiltresController extends AbstractController
 {
     #[Route('/livres/search', name: 'livres_search')]
-    public function livresSearch(Request $req, LivreRepository $rep): Response
+    public function livresSearch(Request $req, 
+                                LivreRepository $rep,
+                                SerializerInterface $serializer): Response
     {
 
         $form = $this->createForm(SearchFiltreLivresType::class);
@@ -21,19 +24,12 @@ class FormSearchLivresFiltresController extends AbstractController
 
         // gestion du submit du form
         if ($form->isSubmitted()){
-        
-            // dd($form->getData());
-
-            // $livres = $rep->findAll();
-
             $livres = $rep->livresEntreDeuxPrix ($form->getData());
-
-            $vars = ['livres' => $livres];
-
-            // ne rendez pas une vue ici, la bonne pratique est de ré-diriger vers une action
-            // return $this->render ('form_search_livres_filtres/livres_search_afficher.html.twig', $vars);
-            return $this->redirectToRoute('livres_search_afficher_resultat');
-  
+            // dump ($form->getData()); // les filtres
+            // dd ($livres); // le résultat de la requête
+            $livresJson = $serializer->serialize($livres,'json');
+            // dd ($livresJson); // le résultat de la requête encodé en JSON
+            return new Response($livresJson);
         }
 
         $vars = ['form' => $form];
